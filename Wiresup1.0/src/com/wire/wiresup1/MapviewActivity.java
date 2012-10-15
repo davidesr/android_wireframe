fa8663c3cf944b01d71632f34f379c57610cc3fd
mameunie <mameunie@FR404373.corp.capgemini.com>
2012-10-15 11:54:30 +0200

    Ajout de MapView

---
+++ Wiresup1.0/src/com/wire/wiresup1/MapviewActivity.java
@@ -0,0 +1,25 @@
+package com.wire.wiresup1;
+
+import android.os.Bundle;
+
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+
+public class MapviewActivity extends MapActivity {
+
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		//ajout d'un commentaire
+		return false;
+	}
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+	    super.onCreate(savedInstanceState);
+	    setContentView(R.layout.activity_mapview);
+	    
+	    MapView mapView = (MapView) findViewById(R.id.mapview);
+	    mapView.setBuiltInZoomControls(true);
+	}
+}
