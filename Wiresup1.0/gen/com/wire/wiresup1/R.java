bc31724dc275d395885a1f10656d3e8ad3ffb3f9
davidesr <davidjean.labs@gmail.com>
2012-10-17 21:48:21 +0200

    Untrack

--- Wiresup1.0/gen/com/wire/wiresup1/R.java
+++ Wiresup1.0/gen/com/wire/wiresup1/R.java
@@ -35,37 +35,38 @@ public final class R {
         public static final int wiresuplogo=0x7f020015;
     }
     public static final class id {
+        public static final int LinearLayout1=0x7f070009;
         public static final int back=0x7f070000;
         public static final int help=0x7f070008;
-        public static final int image=0x7f07000e;
+        public static final int image=0x7f07000f;
         public static final int login=0x7f070005;
         public static final int mapview=0x7f070002;
-        public static final int menu_add=0x7f070011;
-        public static final int menu_logout=0x7f070014;
-        public static final int menu_reminder=0x7f070013;
-        public static final int menu_search=0x7f070012;
-        public static final int menu_settings=0x7f070010;
+        public static final int menu_add=0x7f070012;
+        public static final int menu_logout=0x7f070015;
+        public static final int menu_reminder=0x7f070014;
+        public static final int menu_search=0x7f070013;
+        public static final int menu_settings=0x7f070011;
         public static final int password=0x7f070006;
-        public static final int progressBar=0x7f07000a;
-        public static final int splash=0x7f070009;
+        public static final int progressBar=0x7f07000b;
+        public static final int splash=0x7f07000a;
         public static final int terms=0x7f070003;
-        public static final int text=0x7f07000f;
-        public static final int toast_layout_root=0x7f07000d;
+        public static final int text=0x7f070010;
+        public static final int toast_layout_root=0x7f07000e;
         public static final int validate=0x7f070007;
-        public static final int webView1=0x7f07000c;
+        public static final int webView1=0x7f07000d;
         public static final int webview_help=0x7f070001;
-        public static final int webview_terms=0x7f07000b;
+        public static final int webview_terms=0x7f07000c;
         public static final int wirelogo=0x7f070004;
     }
     public static final class layout {
         public static final int activity_help=0x7f030000;
         public static final int activity_home=0x7f030001;
         public static final int activity_main=0x7f030002;
-        public static final int activity_splashscreen=0x7f030003;
-        public static final int activity_terms_conditions=0x7f030004;
-        public static final int activity_webview=0x7f030005;
-        public static final int toast_layout=0x7f030006;
-        public static final int activity_mapview=0x7f030007;
+        public static final int activity_mapview=0x7f030003;
+        public static final int activity_splashscreen=0x7f030004;
+        public static final int activity_terms_conditions=0x7f030005;
+        public static final int activity_webview=0x7f030006;
+        public static final int toast_layout=0x7f030007;
     }
     public static final class menu {
         public static final int activity_main=0x7f060000;
